d60222ae240968583c08f1eed977b84d4910a777
i1-kmv <i1-kmv@mail.ru>
2021-02-25 12:46:56 +0300

    5.1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, {useState} from 'react';
-import {Header} from "./components/Header";
-import {Categories} from "./components/Categories";
-import {SortPopup} from "./components/SortPopup";
-import {Home} from "./pages/Home";
-import {Route} from "react-router-dom";
-import {Cart} from "./pages/Cart";
+import React, {useState} from 'react'
+import {Header} from "./components/Header"
+import {Home} from "./pages/Home"
+import {Route} from "react-router-dom"
+import {Cart} from "./pages/Cart"
 
 
 function App() {
+
+    const [pizzas, setPizzas] = useState([])
+
+    React.useEffect(() => {
+       fetch(`http://localhost:3000/db.json`).then((resp) => resp.json()).then(
+           json => {
+               setPizzas(json.pizzas)
+           }
+       )
+    }, [])
 
     return (
         <>
             <div className="wrapper">
                 <Header/>
                 <div className="content">
-                    <Route exact path='/' component={Home}/>
-                    <Route exact path='/cart' component={Cart}/>
+                    <Route exact path='/' render={() => <Home items={pizzas}/>}/>
+                    <Route exact path='/cart' render={() => <Cart/>}/>
                 </div>
             </div>
         </>
