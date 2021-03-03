a0de9f652cf732cf1c1a2400df1635709c931281
i1-kmv <i1-kmv@mail.ru>
2021-03-03 22:13:36 +0300

    5.2

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './scss/app.scss';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
 
 
 ReactDOM.render(
     <BrowserRouter>
+        <Provider store={store}>
       <App/>
+        </Provider>
     </BrowserRouter>
     ,document.getElementById('root')
 );
